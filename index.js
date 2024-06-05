ac075f0a2ed3da13c49a1473e792bb659f9c8933
KaranManral <karanmnaral2000@gmail.com>
2024-06-05 22:51:54 +0530

    user agent added

--- index.js
+++ index.js
@@ -82,6 +82,7 @@ const runSeleniumScript = async () => {
     option.addArguments("--disable-gpu");
     option.addArguments("--window-size=1920,1080");
     option.addArguments(`--proxy-server=http://${newProxyHost}:${newProxyPort}`);
+    option.addArguments(['user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"'])
 
     let driver = await new Builder()
     .forBrowser(Browser.CHROME)
